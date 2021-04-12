32edad9cf35c9a7b18dca28761e5631599ab044e
Marcel Stadelmann <m@marceli.to>
2021-04-12 14:50:30 +0200

    fix bug in task message

--- app/Tasks/Message.php
+++ app/Tasks/Message.php
@@ -11,21 +11,30 @@ class Message
     foreach($recipients->all() as $r)
     {
       $courseEvent = \App\Models\CourseEvent::with('course', 'dates')->find($r->message->course_event_id);
-      \Mail::to($r->email)
-            ->bcc(\Config::get('sipt.email_copy'))
-            ->send(
-              new \App\Mail\Message(
-                [
-                  'recipient' => $r,
-                  'message' => $r->message,
-                  'courseEvent' => $courseEvent,
-                ]
-            )
-      );
-
-      // Mark recipient
-      $r->is_sent = 1;
-      $r->save();
+      if ($courseEvent)
+      {
+	      \Mail::to($r->email)
+	            ->bcc(\Config::get('sipt.email_copy'))
+	            ->send(
+	              new \App\Mail\Message(
+	                [
+	                  'recipient' => $r,
+	                  'message' => $r->message,
+	                  'courseEvent' => $courseEvent,
+	                ]
+	            )
+	      );
+	
+	      // Mark recipient
+	      $r->is_sent = 1;
+	      $r->save();	      
+      }
+      else
+      {
+	      \Log::error('Course Event ' . $r->message->course_event_id . ' not found!');
+				$r->is_sent = 1;
+	      $r->save();	  
+      }
     }
   }
 }
