d0b8efc2e5ab8d9c42eadd0472947c33130504ce
Marcel Stadelmann <m@marceli.to>
2021-11-22 18:11:59 +0100

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,7 +38,6 @@ class Kernel extends ConsoleKernel
       $schedule->call(new SymposiumBills)->everyMinute();
       $schedule->call(new CourseEventReminder)->everyMinute();
       $schedule->call(new Message)->everyMinute();
-
       // Newsletters, Advertisements, Notices
       // $schedule->call(new Newsletter)->everyMinute();
       // $schedule->call(new SymposiumNews)->everyMinute();
--- config/sanctum.php
+++ config/sanctum.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,sipt.ch.local,sipt.ch.marceli.to,sipt.ch,psychotraumatologie-sipt.ch,www.psychotraumatologie-sipt.ch')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,sipt.ch.local,sipt.ch.marceli.to,sipt.ch,psychotraumatologie-sipt.ch,www.psychotraumatologie-sipt.ch,staging.sipt.ch,www.staging.sipt.ch')),
 
     /*
     |--------------------------------------------------------------------------
