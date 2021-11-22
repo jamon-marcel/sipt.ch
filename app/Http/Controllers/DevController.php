26ad0e3920ecfc581af9dc1226a55d7396864ce4
Marcel Stadelmann <m@marceli.to>
2021-11-22 09:23:18 +0100

    update

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -3,25 +3,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\SymposiumSubscriber;
+use App\Models\ImportantNoticeSubscriber;
+use App\Models\AdvertismentSubscriber;
+use App\Models\NewsletterSubscriber;
 
 class DevController extends Controller
 {
-  public function __construct(User $user, SymposiumSubscriber $subscriber)
+  public function __construct(
+    User $user, 
+    SymposiumSubscriber $symposiumSubscriber,
+    ImportantNoticeSubscriber $importantNoticeSubscriber,
+    AdvertismentSubscriber $advertismentSubscriber,
+    NewsletterSubscriber $newsletterSubscriber
+  )
   {
     $this->user = $user; 
-    $this->subscriber = $subscriber;  
+    $this->symposiumSubscriber = $symposiumSubscriber; 
+    $this->importantNoticeSubscriber = $importantNoticeSubscriber; 
+    $this->advertismentSubscriber = $advertismentSubscriber; 
+    $this->newsletterSubscriber = $newsletterSubscriber; 
+
   }
 
   public function maskUser()
   {
-    $subscribers = $this->subscriber->get();
-    foreach($subscribers as $u)
-    {
-      $rand_user = \Str::random(8);
-      $u->email = strtolower($rand_user) . '@jamondigital.ch';
-      $u->save();
-    }
 
+    // Users
     $users = $this->user->get();
     foreach($users as $u)
     {
@@ -32,5 +39,42 @@ class DevController extends Controller
         $u->save();
       }
     }
+
+    // Symposium subscribers
+    $symposiumSubscriber = $this->symposiumSubscriber->get();
+    foreach($symposiumSubscriber as $u)
+    {
+      $rand_user = \Str::random(8);
+      $u->email = strtolower($rand_user) . '@jamondigital.ch';
+      $u->save();
+    }
+
+    // Important notice subscribers
+    $importantNoticeSubscribers = $this->importantNoticeSubscriber->get();
+    foreach($importantNoticeSubscribers as $u)
+    {
+      $rand_user = \Str::random(8);
+      $u->email = strtolower($rand_user) . '@jamondigital.ch';
+      $u->save();
+    }
+
+    // Advertisement subscribers
+    $advertismentSubscribers = $this->advertismentSubscriber->get();
+    foreach($advertismentSubscribers as $u)
+    {
+      $rand_user = \Str::random(8);
+      $u->email = strtolower($rand_user) . '@jamondigital.ch';
+      $u->save();
+    }
+
+    // Newsletter subscribers
+    $newsletterSubscribers = $this->newsletterSubscriber->get();
+    foreach($newsletterSubscribers as $u)
+    {
+      $rand_user = \Str::random(8);
+      $u->email = strtolower($rand_user) . '@jamondigital.ch';
+      $u->save();
+    }
+
   }
 }
