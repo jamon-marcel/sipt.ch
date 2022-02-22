2b4111dda8a0e6560e446a2a28153a4541b7687f
Marcel Stadelmann <m@marceli.to>
2022-02-22 10:29:15 +0100

    bf

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,7 +71,7 @@ class User extends Authenticatable implements MustVerifyEmail
   
   public function student()
 	{
-    return $this->belongsTo('App\Models\Student');
+    return $this->hasOne('App\Models\Student', 'user_id', 'id');
   }
   
   public function administrator()
