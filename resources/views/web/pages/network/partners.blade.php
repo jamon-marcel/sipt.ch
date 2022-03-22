ad6f6deed8b9bc7b896268deb72fb793edd320be
Marcel Stadelmann <m@marceli.to>
2022-03-22 09:48:13 +0100

    update partner

--- resources/views/web/pages/network/partners.blade.php
+++ resources/views/web/pages/network/partners.blade.php
@@ -12,7 +12,8 @@
       <a href="javascript:;" class="btn-collapsible js-clpsbl-btn">Deutsches Institut für Psychotraumatologie (DIPT)</a>
     </h2>
     <div class="collapsible__content js-clpsbl-body">
-      <p>Springen 26<br>53804 Much (Nähe Köln)<br>Telefon: 02245 91 94 0 (Dienstag und Donnerstag 10-12 Uhr)<br>Fax: 02245 91 94 10<br><a href="mailto:anfragen@psychotraumatologie.de">anfragen@psychotraumatologie.de</a></p>
+      <p>Springen 26<br>53804 Much (Nähe Köln)<br>Telefon: 0049 2245 919 40 (Dienstag und Donnerstag 10-12 Uhr)<br>Fax: 0049 2245 91 94 10<br><a href="mailto:anfragen@psychotraumatologie.de">anfragen@psychotraumatologie.de</a><br><a href="http://psychotraumatologie.de" target="_blank">www.psychotraumatologie.de</a></p>
+      <p>1991 als gemeinnütziger Verein gegründet, ist das Deutsche Institut für Psychotraumatologie (DIPT) eine der ersten wissenschaftlich-therapeutischen Einrichtungen der Bundesrepublik Deutschland, die sich programmatisch der Erforschung, Psychotherapie und Prävention psychotraumatischer Störungen gewidmet hat. So konnte im Laufe der Zeit eine Sensibilisierung der Fachwelt und der Öffentlichkeit für Psychotraumatologie erreicht werden. In enger Kooperation mit dem Institut für Klinische Psychologie und Psychotherapie der Universität zu Köln unter der Leitung von Prof. Dr. Gottfried Fischer, wurde im Jahr 1995 im Rahmen des Kölner Opferhilfemodell-Projekts (KOM) die psychologische Beratungsstelle für Gewalt- und Unfallopfer ins Leben gerufen und seither systematisch ausgebaut.</p>
     </div>
   </article>
   <article class="collapsible is-expanded js-clpsbl">
@@ -20,9 +21,8 @@
       <a href="javascript:;" class="btn-collapsible js-clpsbl-btn">Zentrum für Trauma- und Konfliktmangement (ZTK)</a>
     </h2>
     <div class="collapsible__content js-clpsbl-body">
-      <p>Clemensstr. 5-7 50676 Köln<br><a href="mailto:info@ztk-koeln.de">info@ztk-koeln.de</a><br><a href="http://www.ztk-koeln.de" target="_blank" rel="noopener">www.ztk-koeln.de</a></p>
-      <p>Das Zentrum für Trauma- und Konfliktmanagement (ZTK) GmbH ist ein interdisziplinärer und qualifizierter Zusammenschluss von Fachleuten unterschiedlicher Berufsgruppen aus den Bereichen der Psychologie, Psychotherapie, Psychiatrie, Sozialpädagogik und dem Rechtswesen. Die Mitarbeitenden sind seit vielen Jahren auf die vielfältigen Arbeitsbereiche der Psychotraumatologie und des Krisen- und Konfliktmanagements spezialisiert. Das ZTK arbeitet nach einem integrativen und bedarfsorientierten Ansatz, der aus den langjährigen Erfahrungen in den genannten Arbeitsbereichen resultiert und sich an aktuellen wissenschaftlichen Erkenntnissen und Forschungsergebnissen orientiert.</p>
-      <p>Das ZTK hat einen eigenen <a href="https://www.edigo-verlag.de/" target="_blank" rel="noopener" class="anchor">Verlag</a>. Der edigo Verlag ist ein unabhängiger, im Jahr 2020 gegründeter Sachbuchverlag, der aktuelle psychologische und gesellschaftspolitische Themen behandelt.<br>Im Alltag stoßen wir auf das Dilemma der verkürzten Botschaft: Informationszyklen werden schneller, Debatten sind geprägt von oberflächlicher Erregung, die Aufmerksamkeitsspanne wird kürzer – und das in hochpolitischen Zeiten. Der edigo Verlag möchte neue Einblicke geben und Hintergrundwissen vermitteln.</p>
+      <p>Clemensstr. 5-7<br>50676 Köln<br><a href="mailto:info@ztk-koeln.de">info@ztk-koeln.de</a><br><a href="http://www.ztk-koeln.de" target="_blank" rel="noopener">www.ztk-koeln.de</a></p>
+      <p>Das Zentrum für Trauma- und Konfliktmanagement (ZTK) ist ein interdisziplinärer und qualifizierter Zusammenschluss von Fachleuten unterschiedlicher Berufsgruppen aus den Bereichen Psychologie, Psychotherapie, Psychiatrie, Sozialpädagogik und Rechtswesen. Die Mitarbeitenden sind seit vielen Jahren auf die vielfältigen Arbeitsbereiche der Psychotraumatologie und des Krisen- und Konfliktmanagements spezialisiert. Das ZTK arbeitet nach einem integrativen und bedarfsorientierten Ansatz, der aus den langjährigen Erfahrungen in den genannten Arbeitsbereichen resultiert und sich an aktuellen wissenschaftlichen Erkenntnissen und Forschungsergebnissen orientiert.<br>Das ZTK hat einen eigenen <a href="https://www.edigo-verlag.de/" target="_blank" class="underline">Verlag</a>. Der edigo Verlag ist ein unabhängiger, im Jahr 2020 gegründeter Sachbuchverlag, der aktuelle psychologische und gesellschaftspolitische Themen behandelt.<br>Im Alltag stoßen wir auf das Dilemma der verkürzten Botschaft: Informationszyklen werden schneller, Debatten sind geprägt von oberflächlicher Erregung, die Aufmerksamkeitsspanne wird kürzer – und das in hochpolitischen Zeiten. Der edigo Verlag möchte neue Einblicke geben und Hintergrundwissen vermitteln.</p>
     </div>
   </article>
   <article class="collapsible is-expanded js-clpsbl">
@@ -31,7 +31,42 @@
     </h2>
     <div class="collapsible__content js-clpsbl-body">
       <p>Neumarkt 4<br>8400 Winterthur<br>Telefon: 052 208 03 20<br><a href="mailto:info@kriseninterventionschweiz.ch">info@kriseninterventionschweiz.ch</a></p>
-      <p>Die Krisenintervention Schweiz, eine zertifizierte Ausbildungs- und Einsatzorganisation NNPN, bietet neben Beratung in Krisen- und Notsituationen sowohl in Betrieben als auch im pädagogischen Bereich Aus- und Fortbildungenbildungen in praxisbezogener Notfall-Psychologie für Peers und Caregiver an. (TeilnehmerInnen, die bei der Krisenintervention Schweiz eine Peer-/Careausbildung absolviert haben, werden beim SIPT die Module „Grundkurs“ und „Akute Traumatisierung“ angerechnet.) Die Krisenintervention Schweiz bietet in herausfordernden Situationen Beratungen für Teams, Führungs- und Privatpersonen an.</p>
+      <p>Die Krisenintervention Schweiz, eine zertifizierte Ausbildungs- und Einsatzorganisation NNPN, bietet in herausfordernden Situationen Beratungen für Teams, Führungs- und Privatpersonen an – sowohl in Betrieben als auch im pädagogischen Bereich. Neben dem Beratungsangebot in Krisen- und Notsituationen, stellt die Krisenintervention Schweiz auch Aus- und Fortbildungen in praxisbezogener Notfall-Psychologie für Peers und Caregiver bereit. (Teilnehmenden, die bei der Krisenintervention Schweiz eine Peer-/Careausbildung absolviert haben, werden beim SIPT die Module „Grundkurs“ und „Akute Traumatisierung“ angerechnet.)</p>
+    </div>
+  </article>
+  <article class="collapsible is-expanded js-clpsbl">
+    <h2>
+      <a href="javascript:;" class="btn-collapsible js-clpsbl-btn">Marie Meierhofer Institut für das Kind (MMI)</a>
+    </h2>
+    <div class="collapsible__content js-clpsbl-body">
+      <p>Pfingstweidstrasse 16<br>8005 Zürich<br>Telefon: 044 205 52 20<br><a href="mailto:info@mmi.ch">info@mmi.ch</a><br><a href="https://www.mmi.ch/de-ch/" target="_blank">www.mmi.ch</a></p>
+      <p>Das Marie Meierhofer Institut (MMI) für das Kind ist eine unabhängige Fachinstitution für die frühe Kindheit. Im Zentrum der Tätigkeit steht das Kind: seine Entwicklung, seine Lebensumstände, seine Bedürfnisse und seine Rechte. Das MMI vereint entwicklungspsychologisches, frühpädagogisches und familiensoziologisches Wissen und sozialwissenschaftliche, beraterische, psychotherapeutische Kompetenzen. Es verbindet Praxis und Forschung in einer multi- und transdisziplinären Arbeitsweise. Es bietet Dienstleistungen an und setzt gesellschaftlich relevante Forschungs- und Praxisprojekte um. Aufgrund seiner Expertise und seiner Arbeitsweise ist das MMI auch ein assoziiertes Institut der Universität Zürich.</p>
+    </div>
+  </article>
+  
+  <article class="collapsible is-expanded js-clpsbl">
+    <h2>
+      <a href="javascript:;" class="btn-collapsible js-clpsbl-btn">Zentrum für interdisziplinäre Therapien (ZiT)</a>
+    </h2>
+    <div class="collapsible__content js-clpsbl-body">
+      <p>
+      Obere Laube 44<br>
+  78462 Konstanz<br>
+  Deutschland<br>
+  Telefon: 0049 7531 915501<br>
+  <a href="mailto:info@myoreflex.de">info@myoreflex.de</a><br><a href="https://www.myoreflex.de " target="_blank">www.myoreflex.de</a><br><a href="https://www.myoreflex.de/uebersichtsseite-praxen/zit-konstanz " target="_blank">www.myoreflex.de/uebersichtsseite-praxen/zit-konstanz</a></p>
+      </p>
+      <p>Im Mittelpunkt der Myoreflextherapie steht das Zentrum für interdisziplinäre Therapien in Konstanz (ZiT). In dieser Gemeinschaftspraxis finden sich die verschiedensten Therapieformen unter einem Dach: Von der Myoreflextherapie über westliche Hochschulmedizin bis zur traditionellen chinesischen Medizin, von der Klangschalenbehandlung bis zur Psychotherapie.<br>Eine der wichtigsten Säulen der Myoreflextherapie ist die produktive und vertrauensvolle Zusammenarbeit zwischen Patienten, Therapeuten, dem Umfeld sowie ein lebendiges Netzwerk von Bündnispartnern – zu denen auch das SIPT gehört.</p>
+    </div>
+  </article>
+  
+  <article class="collapsible is-expanded js-clpsbl">
+    <h2>
+      <a href="javascript:;" class="btn-collapsible js-clpsbl-btn">Gesundheits-Campus der St. Elisabeth Universität (SEU)</a>
+    </h2>
+    <div class="collapsible__content js-clpsbl-body">
+      <p>Bratislava, mit Sitz in Luzern<br><a href="https://gesundheits-campus.org/91/kontakt" target="_blank">gesundheits-campus.org/91/kontakt</a></p>
+      <p>Die St. Elisabeth Universität (SEU) ist eine staatlich anerkannte H+ Hochschule in Bratislava mit privater Trägerschaft. Die SEU bietet zielgruppenspezifische Studiengänge, Weiterbildungen (CAS, DAS, MAS) und Doktoratsstudienprogramme (PhD) für Gesundheits- und Sozialberufe an. Der Gesundheits-Campus mit Sitz in Ebikon/Luzern (CH) kann sowohl auf die bereits akkreditierten Studiengänge (Bachelor- und Masterstudiengänge) und Doktoratsstudienprogramme (PhD) der SEU zurückgreifen, als auch eigene Weiterbildungsstudiengänge im Rahmen „Gesundheit und Soziales“ entwerfen. Diese neuen Bildungsmöglichkeiten werden durch die SEU akkreditiert/genehmigt, und durch den Gesundheits-Campus im deutschsprachigen Europa (Schweiz, Deutschland, Österreich, Liechtenstein und Luxemburg) angeboten. Der Gesundheits-Campus der SEU ist die offizielle und anerkannte Außenstelle für das deutschsprachige Europa.</p>
     </div>
   </article>
 </section>
