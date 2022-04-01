125bfae54637ac6f5ec25831ad5c0df7f8242cce
Marcel Stadelmann <m@marceli.to>
2022-04-01 16:38:42 +0200

    create important notice

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,11 +39,11 @@ class Kernel extends ConsoleKernel
       $schedule->call(new CourseEventReminder)->everyMinute();
       $schedule->call(new Message)->everyMinute();
       $schedule->call(new Newsletter)->everyMinute();
-      
+      $schedule->call(new ImportantNotice)->everyMinute();
+
       // Newsletters, Advertisements, Notices
       // $schedule->call(new SymposiumNews)->everyMinute();
       // $schedule->call(new Advertisment)->everyMinute();
-      // $schedule->call(new ImportantNotice)->everyMinute();
     }
   }
 
--- resources/views/mails/important-notice/index.blade.php
+++ resources/views/mails/important-notice/index.blade.php
@@ -1,7 +1,9 @@
 @component('mail::message')
-<p style="text-align:right">Winterthur, {{ date('d', time())}}. September 2021</p>
+<p style="text-align:right">Winterthur, {{ date('d', time())}}. April 2022</p>
 <p>Liebe Kolleginnen, liebe Kollegen</p>
-<p>Aufgrund der neuen Bestimmungen des BAG besteht für alle unsere Kurse Zertifikatsplicht. Gefordert ist ein Zertifikat mit persönlichem Ausweis.</p>
-<p>Wir versuchen für die TeilnehmerInnen, die nicht geimpft sind, eine Beischaltung über Zoom einzurichten.</p>
+<p>Gerne teile ich Ihnen mit, dass ab 1. April Dr. phil. Maria Teresa Diez Grieser die Leitung des Bereichs Kinder und Jugendliche beim SIPT übernimmt. Ihre Aufgabe wird die Stärkung und der Ausbau des Kinder- und Jugendlichenbereichs sein.</p>
+<p>Einige von Ihnen kennen Sie sicher von Ihren Kursen am SIPT. Maria Teresa ist seit ca. fünf Jahren bereits beim SIPT als Dozentin tätig. Sie ist nicht nur eine ausgezeichnete Klinikerin, sondern hat auch eine langjährige Lehr- und Forschungserfahrung.</p>
+<p>Für alle Fragen, die die Traumapädagogik betreffen, wird Maria Teresa Diez Grieser für Sie Ansprechpartnerin sein. So können wir schneller und gezielter auf Ihre Anliegen eingehen.</p>
+<p>Ich freue mich sehr mit ihr eine kompetente Fachperson gefunden zuhaben, die der Traumapädagogik im SIPT sicher neue Anstösse gibt und wünsche Ihr einen guten Start!</p>
 <p>Herzliche Grüsse<br>Rosmarie Barwinski<br>Leitung SIPT</p>
 @endcomponent
