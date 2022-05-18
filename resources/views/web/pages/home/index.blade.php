bf39b97bd0f9e7ed4cdc74df0653f85dc52d5939
Marcel Stadelmann <m@marceli.to>
2022-05-18 16:17:39 +0200

    update homepage

--- resources/views/web/pages/home/index.blade.php
+++ resources/views/web/pages/home/index.blade.php
@@ -43,18 +43,18 @@
   </article>
   <article>
     <h2>Neue Kursangebote</h2>
-    <p>Gerne weisen wir auf <strong>aktuelle</strong> Kursangebote hin, die Sie unter „Spezielle Themen der Psychotraumatologie“ finden können:</p>
+    <p>Gerne weisen wir auf <strong>aktuelle</strong> Kursangebote hin:</p>
     <p>
-      <strong>Migration und Trauma – transkulturelle und traumatherapeutische Kompetenzen in der Behandlung und Beratung von Geflüchteten und Menschen mit Migrationshintergrund</strong><br>Dozent: Oliver Schwald<br>Termin:  1./2. April 2022
+      <strong>Vulnerable Kinder und Jugendliche mit Migrationshintergrund kompetent begleiten und unterstützen</strong><br>Dozent: Silvan Holzer<br>Termin:  2./3. September 2022
+      <br><a href="/bildungsangebot/modul/vulnerable-kinder-und-jugendliche-mit-migrationshintergrund-kompetent-begleiten-und-unterstuetzen-fakultativ/b3d6d972-5aa2-4f2d-a767-9f67e4b92b5b" class="icon-arrow-right">mehr erfahren</a>
+    </p>
+    <p>
+      <strong>Traumatisierte Flüchtlinge im Schulalltag</strong><br>Dozentin: Ruth Monstein<br>18./19. November 2022
+      <br><a href="/bildungsangebot/modul/traumatisierte-fluechtlinge-im-schulalltag-fakultativ/31a9bb59-80ef-4a12-8228-81ba17a061bd" class="icon-arrow-right">mehr erfahren</a>
+    </p>
+    <p>
+      <strong>Migration und Trauma – transkulturelle und traumatherapeutische Kompetenzen in der Behandlung und Beratung von Geflüchteten und Menschen mit Migrationshintergrund</strong><br>Dozentinnen: Oliver Schwald<br>2./3. Dezember 2022
       <br><a href="/bildungsangebot/modul/migration-und-trauma-transkulturelle-und-traumatherapeutische-kompetenzen-in-der-behandlung-und-beratung-von-gefluechteten-und-menschen-mit-migrationshintergrund-fakultativ/a9690678-91f1-4531-8c49-e4ecec12d088" class="icon-arrow-right">mehr erfahren</a>
-    </p>
-    <p>
-      <strong>Traumatisierte Flüchtlinge im Schulalltag</strong><br>Dozentin: Ruth Monstein<br>13./14. Mai 2022
-      <br><a href="/bildungsangebot/modul/traumatisierte-fluechtlinge-im-schulalltag/31a9bb59-80ef-4a12-8228-81ba17a061bd" class="icon-arrow-right">mehr erfahren</a>
-    </p>
-    <p>
-      <strong>Online-Seminar: Einführung in die Psychotraumatologie und Traumapädagogik</strong><br>Dozentinnen: Maria Teresa Diez Grieser/Rosmarie Barwinski<br>11. Mai 2022
-      <br><a href="/bildungsangebot/modul/online-seminar-einfuehrung-in-die-psychotraumatologie-und-traumapaedagogik/ef5f55f7-23c4-4a49-9f91-e8749793707c" class="icon-arrow-right">mehr erfahren</a>
     </p>
   </article>
 </section>
