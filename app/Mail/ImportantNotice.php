fd0b5ac6f5aa05c650bec50951f5f1d6dc6e0b30
Marcel Stadelmann <m@marceli.to>
2022-05-24 14:59:51 +0200

    add flyer as attachment

--- app/Mail/ImportantNotice.php
+++ app/Mail/ImportantNotice.php
@@ -28,6 +28,7 @@ class ImportantNotice extends Mailable
   public function build()
   {
     $mail = $this->subject('SIPT – Fachtagung am 26./27.08.2022 in Köln')->markdown('mails.important-notice.index');
+    $mail->attach(public_path() . '/storage/downloads/' . 'Flyer-Fachtagung-2022', ['mime' => 'application/pdf']);
     return $mail;
   }
 }
