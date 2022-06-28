8ea10ae4551e0ab1abf141d2580e4f72c473f9f0
Marcel Stadelmann <m@marceli.to>
2022-06-28 13:50:41 +0200

    update newsletter

--- resources/views/mails/newsletter/book.blade.php
+++ resources/views/mails/newsletter/book.blade.php
@@ -1,8 +1,17 @@
 @component('mail::message')
-# Neues Buch – Orientierung für den komplexen Prozess der Traumabehandlung
+# 20 Jahre Zeitschrift Trauma - Zeitschrift für Psychotraumatologie und ihre Anwendungen (Asanger Verlag)
 <p>Liebe Kolleginnen, liebe Kollegen</p>
-<p>Gerne mache ich Sie auf mein neues Buch aufmerksam, dass vor einer Woche bei Klett-Cotta erschienen ist.</p>
+<p>Die Zeitschrift Trauma des Asanger Verlages wird 20 Jahre alt.</p>
+<p>Leider fällt das Jubiläum in eine Zeit, in der durch den Krieg in der Ukraine erneut schwer erträgliche Traumatisierungen vor unseren Augen stattfinden. Die Aktualität des Themas unserer Zeitschrift ist größer denn je. 2003 gegründet, ist sie die erste deutschsprachige Traumazeitschrift.</p>
+<p>„Trauma“ thematisiert psychische Ausnahmesituationen, ausgelöst durch einmalige überwältigende Ereignisse wie Terroranschläge, Naturkatastrophen und Kriege, Schockerfahrungen durch Unfälle, Erkrankungen und Kontrollverlust sowie andauernde oder sich wiederholende körperliche, psychische und sexuelle Gewalt in der Familie und in kirchlichen, schulischen und sozialen Einrichtungen.</p>
+<p>Die von Expertinnen und Experten, aber auch von Traumaopfern und Überlebenden verfassten Beiträge über Ursachen, Therapie- und Präventionsansätze richten sich nicht nur an Studierende der Psychologie, Medizin oder Soziologie, an Psychologische und Ärztliche Psychotherapeut:innen, Psychiater:innen, Fachärzt:innen, Mitarbeiter:innen von Beratungsstellen und sozialpädagogischen Einrichtungen oder Sozialarbeiter, Lehrerkräfte und Erzieher:innen. Ebenso wird die Gefahr der sekundären und indirekten Traumatisierung in der Seelsorge, im Justizbereich (Richter, Rechts- und Staatsanwälte), in „Blaulichtberufen“ (Feuerwehr, Polizei, Rettungsdienste) und bei nahestehenden Personen und Familienangehörigen thematisiert.</p>
+<p>„Trauma“ ist weder inhaltlich-thematisch noch organisatorisch an eine therapeutische oder traumatherapeutische Fachgesellschaft oder Institution gebunden und repräsentiert ein breites Spektrum theoretischer und klinischer Zugänge.</p>
+<p>Herausgeber von „Trauma“ sind Prof. Dr. phil. Rosmarie Barwinski (Schweizerisches Institut für Psychotraumatologie, Zürich), Prof. Dr. med. Robert Bering (Chefarzt, Regionspsychiatrie West, Gødstrup, Dänemark; Universität zu Köln, Department für Heilpädagogik und Rehabilitation); Prof. Dr. med. Manfred Sauer (Freiburg), Prof. Dr.med. Carl Eduard Scheidt (Klinik für Psychosomatische Medizin und Psychotherapie Universitätsklinikum, Freiburg), Dipl.-Psych. Thomas Weber (Zentrum für Trauma- und Konfliktmanagement, Köln).</p>
+<p>PDF des Verlages zur Zeitschrift:<br><a href="https://www.asanger.de/downloads/traumaflyer-20-seiter-22-05neupdf.pdf" target="_blank">asanger.de/downloads/traumaflyer</a></p>
+<p>Link zum Verlag (Abonnement):<br><a href="https://www.asanger.de/zeitschriftzppm/bestellung/index.php" target="_blank">asanger.de/zeitschriftzppm/bestellung</p>
 <p>Freundliche Grüsse<br><br>Rosmarie Barwinski<br>SIPT Schweizer Institut für Psychotraumatologie</p>
 <br>
 <p style="font-size: 14px; margin-bottom: 0">Sie möchten keine News vom SIPT mehr erhalten? Klicken Sie bitte <span class="break-all"><a href="{{ route('newsletter_cancel', ['newsletterSubscriber' => $subscriber->id]) }}" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</span></p>
 @endcomponent
+
+
