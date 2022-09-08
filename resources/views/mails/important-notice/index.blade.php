c207d089756b5f6f92d440d497d1de28547a2443
Marcel Stadelmann <m@marceli.to>
2022-09-08 10:19:59 +0200

    update mailtext

--- resources/views/mails/important-notice/index.blade.php
+++ resources/views/mails/important-notice/index.blade.php
@@ -1,7 +1,9 @@
 @component('mail::message')
 <p style="text-align:right">Winterthur, {{ date('d', time())}}. September 2022</p>
 <p>Liebe Kolleginnen und Kollegen,</p>
-<p>Gerne möchten wir Sie auf unser neues Kursangebot „So vertiefen Sie Ihr psychodynamisches Fallverständnis“ aufmerksam machen.<br>Mit Prof. Dr. phil. Dipl.-Psych. Ingo Jungclaussen konnten wir einen weiteren neuen Dozenten aus Deutschland engagieren, der im Arbeits- und Forschungsgebiet der neuen didaktischen Formen zur Vermittlung der psychodynamischen Psychotherapie besticht. PsychotherapeutInnen lernen in diesem Seminar praxisnah die drei Konflikt-, Struktur- und Trauma-Leitfäden sowie die psychogenetische Konflikttabelle kennen.<br><br>Weitere Informationen und den Link zur Buchung finden Sie hier auf der SIPT Website.<br><br>Wir hoffen auf Ihr Interesse und würden uns über Ihre Anmeldung freuen.<br><br>
+<p>Gerne möchten wir Sie auf unser neues Kursangebot „So vertiefen Sie Ihr psychodynamisches Fallverständnis“ aufmerksam machen.<br>Mit Prof. Dr. phil. Dipl.-Psych. Ingo Jungclaussen konnten wir einen weiteren neuen Dozenten aus Deutschland engagieren, der im Arbeits- und Forschungsgebiet der neuen didaktischen Formen zur Vermittlung der psychodynamischen Psychotherapie besticht. PsychotherapeutInnen lernen in diesem Seminar praxisnah die drei Konflikt-, Struktur- und Trauma-Leitfäden sowie die psychogenetische Konflikttabelle kennen.</p>
+<p>Weitere Informationen und den Link zur Buchung finden Sie <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/so-vertiefen-sie-ihr-psychodynamisches-fallverstaendnis/469a8a6b-7963-4d47-858b-16498db0410f" target="_blank" style="color: #000; text-decoration: underline;" title="Link Buchung">hier</a> auf der SIPT Website.</p>
+<p>Wir hoffen auf Ihr Interesse und würden uns über Ihre Anmeldung freuen.</p>
 <p>Herzliche Grüsse</p>
 <p>Rosmarie Barwinski</p>
 @endcomponent
