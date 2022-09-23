7f9f4ffa9bf956db4182b317b64499fe7210e3e8
Marcel Stadelmann <m@marceli.to>
2022-09-23 17:07:00 +0200

    add linkedin profile

--- resources/views/web/components/content/contact.blade.php
+++ resources/views/web/components/content/contact.blade.php
@@ -23,6 +23,18 @@
         E-Mail: <a href="mailto:sekretariat@sipt.ch" target="_blank">sekretariat@sipt.ch</a>
       </p>
     </article>
+    <article>
+      <h2>Social Media</h2>
+      <p>
+        Folgen Sie uns in den sozialen Medien<br>
+        <a href="https://www.linkedin.com/company/schweizer-institut-f%C3%BCr-psychotraumatologie" target="_blank" class="icon-social">
+          <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 50 50">
+            <path d="M26.6 22.4s-.1 0 0 0zM45.8 0H3.7C1.6 0 0 1.6 0 3.6v42.6c0 2 1.6 3.6 3.7 3.6h42.1c2 0 3.7-1.6 3.7-3.6V3.6c-.1-2-1.7-3.6-3.7-3.6zM15 41.6H7.5V19.2H15v22.4zm-3.8-25.5c-2.6 0-4.2-1.7-4.2-3.9s1.7-3.9 4.2-3.9c2.6 0 4.1 1.7 4.2 3.9.1 2.2-1.5 3.9-4.2 3.9zm30.7 25.5h-7.5v-12c0-3-1.1-5.1-3.8-5.1-2.1 0-3.3 1.4-3.8 2.7-.2.5-.2 1.1-.2 1.8v12.5h-7.5s.1-20.4 0-22.5h7.5v3.2c1-1.5 2.8-3.7 6.7-3.7 4.9 0 8.6 3.2 8.6 10.1v13z"/>
+          </svg>
+          <span>LinkedIn</span>
+        </a>
+      </p>
+    </article>
     <article class="imprint">
       <p>
         Impressum:<br>
