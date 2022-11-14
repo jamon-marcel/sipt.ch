720ddb6ec0ede6d93489b8eca06432302ba1e9dc
Marcel Stadelmann <m@marceli.to>
2022-11-14 16:26:02 +0100

    update

--- resources/views/mails/newsletter/index.blade.php
+++ resources/views/mails/newsletter/index.blade.php
@@ -1,13 +1,13 @@
 @component('mail::message')
 <p>Liebe Kolleginnen und Kollegen</p>
-<p>Gerne möchten wir Sie auf unser aktuelles Kursangebot „Traumatisierte Flüchtlinge im Schulalltag“ aufmerksam machen.<br>Sie lernen in diesem Kurs, wie sich eine Fluchterfahrung auf Kinder auswirken kann. Zusammen mit unserer erfahrenen Dozentin Ruth Monstein gehen Sie Fragen nach, wie:</p>
+<p>Gerne möchten wir Sie auf unser aktuelles Kursangebot „Traumatisierte Flüchtlinge im Schulalltag“ aufmerksam machen.<br>Sie lernen in diesem Kurs, wie sich eine Fluchterfahrung auf Kinder auswirken kann. Zusammen mit unserer erfahrenen Dozentin <a href="https://psychotraumatologie-sipt.ch/das-sipt/dozent/ruth-monstein/e3cd9421-bafb-4efd-9d33-579917236e70" target="_blank">Ruth Monstein</a> gehen Sie Fragen nach, wie:</p>
 <ul>
 <li>Auf welche Weise zeigen sich Traumatisierungen?</li>
 <li>Welche Chancen bietet ein Schulalltag, um Kinder zu stabilisieren und was fördert ihre Resilienz?</li>
 <li>Welche sensiblen Momente gilt es im Auge zu behalten, damit keine Retraumatisierungen erfolgen?</li>
 </ul>
 <p>Ruth Monstein, Fachberaterin Psychotraumatologie und selbst Primarlehrerin, vermittelt Ihnen praxisnah konkrete Übungen zur Stabilisierung von körperlichem, gedanklichem und emotionalen Stress. Persönliche Selbstfürsorge für Sie wird ebenfalls Inhalt des Seminars sein.</p>
-<p>Weitere Informationen und den Link zur Buchung finden Sie hier auf der SIPT Website.</p>
+<p>Weitere Informationen und den Link zur Buchung finden Sie <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/traumatisierte-fluechtlinge-im-schulalltag-fakultativ/31a9bb59-80ef-4a12-8228-81ba17a061bd" target="_blank">hier</a> auf der SIPT Website.</p>
 <p>Wir hoffen auf Ihr Interesse und würden uns über Ihre Anmeldung freuen.</p>
 <p>Beste Grüsse<br>Rosmarie Barwinski</p>
 <br>
