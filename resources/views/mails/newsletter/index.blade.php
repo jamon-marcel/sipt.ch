adaef1c050020e31c0d692e36855fff5a3f77ccd
Marcel Stadelmann <m@marceli.to>
2022-12-19 16:13:15 +0100

    updates newsletter text

--- resources/views/mails/newsletter/index.blade.php
+++ resources/views/mails/newsletter/index.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 <p>Liebe Kolleginnen und Kollegen</p>
-<p>Mit dem <a href="https://sipt.ch/storage/downloads/sipt-aufbau_08.pdf" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">8. Aufbau-Tipp</a> führt Sie Karin Salm auf dem Etzelpass nach Einsiedeln, begleitet von barocken Meisterwerken.</p>
+<p>Mit dem <a href="https://sipt.ch/storage/downloads/sipt-aufbau_08.pdf" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">8. Aufbau-Tipp</a> führt Sie Karin Salm vom Etzelpass nach Einsiedeln vorbei an barocken Kleinstorten und Spektakeln, die in der Klosterstadt ihren Höhepunkt finden.
 <p>Beste Grüsse<br>Rosmarie Barwinski</p>
 <br>
 <p style="font-size: 14px; margin-bottom: 0">Sie möchten keine News vom SIPT mehr erhalten? Klicken Sie bitte <span class="break-all"><a href="{{ route('newsletter_cancel', ['newsletterSubscriber' => $subscriber->id]) }}" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</span></p>
