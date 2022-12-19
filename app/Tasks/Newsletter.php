106d34838bda6ac60e4357c493d5acefd2905297
Marcel Stadelmann <m@marceli.to>
2022-12-19 18:12:13 +0100

    updates newsletter

--- app/Tasks/Newsletter.php
+++ app/Tasks/Newsletter.php
@@ -16,9 +16,9 @@ class Newsletter
             new \App\Mail\Newsletter(
               [
                 'subscriber'  => $s,
-                // 'attachments' => [
-                //   public_path() . '/storage/downloads/' . 'sipt-aufbau_07.pdf',
-                // ]
+                'attachments' => [
+                  public_path() . '/storage/downloads/' . 'sipt-aufbau_08.pdf',
+                ]
               ]
               )
             );
