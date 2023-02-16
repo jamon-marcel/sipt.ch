32382fa46781246a3268fbf6ba3239c44d646c3b
Marcel Stadelmann <m@marceli.to>
2023-02-16 11:31:21 +0100

    wip

--- app/Console/Commands/ImportMailinglistSubscriber.php
+++ app/Console/Commands/ImportMailinglistSubscriber.php
@@ -39,7 +39,6 @@ class ImportMailinglistSubscriber extends Command
   public function handle()
   {
     $advertisment_subscribers_data = json_decode(file_get_contents(storage_path() . "/app/public/advertisment_subscribers.json"), true);
-    $important_notice_subscribers_data = json_decode(file_get_contents(storage_path() . "/app/public/important_notice_subscribers.json"), true);
     $newsletter_subscribers_data = json_decode(file_get_contents(storage_path() . "/app/public/newsletter_subscribers.json"), true);
 
     // 1. Add all previous subscribers to the list "Newsletter/Aufbautipp"
@@ -75,23 +74,6 @@ class ImportMailinglistSubscriber extends Command
       $this->info('added email: ' . $user['email'] . ' from advertisment_subscribers.json');
     }
 
-    foreach($important_notice_subscribers_data as $user)
-    {
-      // Check if already added
-      $already_added = MailinglistSubscriber::where('email', $user['email'])->first();
-      if (!$already_added)
-      {
-        MailinglistSubscriber::create([
-          'id' => \Str::uuid(),
-          'description' => 'Newsletter/Aufbautipp',
-          'mailinglist_id' => env('MAILINGLIST_NEWSLETTER'),
-          'email' => $user['email'],
-          'is_processed' => 1,
-          'is_confirmed' => 1,
-        ]);
-      }
-      $this->info('added email: ' . $user['email'] . ' from advertisment_subscribers.json');
-    }
     foreach($newsletter_subscribers_data as $user)
     {
       // Check if already added
