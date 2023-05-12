2a0b923903b6ab9f7650d481d11d87265a6fe469
Marcel Stadelmann <m@marceli.to>
2023-05-12 10:17:51 +0200

    wip

---
+++ app/Console/Commands/CleanupMailingListSubscribers.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Console\Commands;
+use App\Models\Mailinglist;
+use App\Models\MailinglistSubscriber;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CleanupMailingListSubscribers extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'cleanup:subscribers';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Compares mailinglist subscribers and deletes them if necessary';
+
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Execute the console command.
+   *
+   * @return int
+   */
+  public function handle()
+  {
+    $subscribers = MailinglistSubscriber::all();
+    foreach($subscribers as $subscriber)
+    {
+      $user = User::where('email', '%@yahoo%', $subscriber->email)->first();
+      if (!$user)
+      {
+        $this->info('No user found with email: ' . $subscriber->email);
+      }
+      else {
+        $this->info('User found with email: ' . $subscriber->email);
+      }
+    }
+  }
+}
