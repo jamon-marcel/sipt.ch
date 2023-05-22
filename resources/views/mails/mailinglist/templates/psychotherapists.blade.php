a25fd49764d05cbf480707d9e1c0bc674a3bc53f
Marcel Stadelmann <m@marceli.to>
2023-05-22 20:47:26 +0200

    wip

--- resources/views/mails/mailinglist/templates/psychotherapists.blade.php
+++ resources/views/mails/mailinglist/templates/psychotherapists.blade.php
@@ -2,7 +2,20 @@
 <p>Liebe Kolleginnen und Kollegen</p>
 <p>Wir bieten folgende neuen Kurse für PsychotherapeutInnen an:</p>
 <p><a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/integration-einer-traumaverarbeitenden-behandlung-mit-emdr-in-ein-psychodynamisches-behandlungskonzept/5cd19b28-f7ca-4d0f-90fc-9ff8ce2b10ea" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">Integration einer traumaverarbeitenden Behandlung mit EMDR in ein psychodynamisches Behandlungskonzept</a>, (Dozentin: Dr. med. Sabine Röcker)</p>
+<p>Am Beispiel von EMDR, einer weltweit anerkannten Methode der Traumakonfrontation, wird aufgezeigt, wie die Arbeit mit einer traumaverarbeitenden Methode in eine psychodynamische Psychotherapie eingebettet werden kann.</p>
+<p>Inhalte des Kurses sind u.a. Folgende Fragen:</p>
+<ul>
+<li>Wie eine Behandlung mit EMDR vorbereitet werden kann: vorbereitende Übungen werden im Kurs demonstriert, bzw. miteinander geübt</li>
+<li>Die Bedeutung von Stabilisierung und Ressourcenaktivierung</li>
+<li>Kontraindikationen?</li>
+<li>Was sind die Bedingungen für den Beginn einer traumaverarbeitenden Behandlungsphase?</li>
+<li>Anhand von Fallbeispielen wird aufgezeigt, wie eine individuelle Behandlungsplanung in Zusammenarbeit mit unseren PatientInnen entstehen kann</li>
+<li>Traumakonfrontation im stationären Kontext oder ambulant? Was sind Kriterien? Vor und Nachteile des jeweiligen Settings.</li>
+</ul>
+<p>Anhand von Fallbeispielen werden diese Fragestellungen im Kurs von Dr. med. Sabine Rocker erläutert.</p>
+<p>&nbsp;</p>
 <p><a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/einfuehrung-in-die-therapie-mit-komplextraumatisierten-patient-innen/5a16ac2d-cf9d-4954-be93-fd630f54fa6c" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">Einführung in die Therapie mit komplextraumatisierten Patient/-innen</a>, (Dozentin: lic. phil. Maya Schenkel)</p>
+<p>Neben den meist vorhandenen komorbiden psychischen und psychiatrischen Störungen ergeben sich bei Menschen, die an einer komplexen Traumafolgestörung leiden, zusätzlich eine Vielfalt von anderen Problemen. Aus der Behandlungspraxis erwies sich ein Abstimmen und Einstimmen auf die aktuelle Gesamtsituation der Patienten als förderlich. Bei Vorliegen einer dissoziativen Persönlichkeitsstruktur muss diese Ausprägung berücksichtigt und aktiv in die Therapie mit einbezogen werden. Eine traumamodifizierte Behandlung wird empfohlen (DeGPT).<br>Es bietet sich die Gelegenheit, die Spezifität des eigenen Verfahrens mit einem störungsspezifischen Ansatz zu erweitern und die Möglichkeit der Reflexion der eigenen therapeutischen Haltung zuzulassen. Die Themen werden von lic. phil. Maya Schenkel praxisnah vermittelt, Fallbeispiele sind willkommen. </p>
 <p>Weitere Informationen finden Sie auf unserer Homepage.</p>
 <p>Herzliche Grüsse<br>Rosmarie Barwinski</p><br>
 <p style="font-size: 14px; margin-bottom: 0">Sie möchten keine News vom SIPT mehr erhalten? Klicken Sie bitte <span class="break-all"><a href="{{ route('mailinglist_cancel', ['mailinglistSubscriber' => $subscriber->id]) }}" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</span></p>
