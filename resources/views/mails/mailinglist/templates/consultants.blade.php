115d2228ec6b81959320f2b97945ebf1f90a6aad
Marcel Stadelmann <m@marceli.to>
2023-06-08 13:23:37 +0200

    wip

--- resources/views/mails/mailinglist/templates/consultants.blade.php
+++ resources/views/mails/mailinglist/templates/consultants.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 <p>Liebe Kolleginnen und Kollegen</p>
-<p>Gerne machen wir Sie auf freie Plätze im Kurs <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/akute-traumatisierung/c56797ea-d47c-44d0-a842-5d5982a733dd" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">«Akute Traumatisierung» am 21./22. Juli 2023 aufmerksam.</a></p>
+<p>Gerne machen wir Sie auf freie Plätze im Kurs <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/akute-traumatisierung/c56797ea-d47c-44d0-a842-5d5982a733dd" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">«Akute Traumatisierung» am 21./22. Juli 2023</a> aufmerksam.</p>
 <p>Unser erfahrener Dozent und Leiter des Zentrums für Trauma- und Konfliktmanagement Thomas Weber führt die Teilnehmenden breit in die zentralen Konzepte der Psychotraumatologie ein, so dass sie ein solides Fundament für die Teilnahme an den stärker spezialisierten Kursen des SIPT gewinnen.</p>
 <p>Sie möchten verschiedene Stabilisierungstechniken sowie Massnahmen bei Akutinterventionen kennenlernen?</p>
 <p>Gerne können Sie sich <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/akute-traumatisierung/c56797ea-d47c-44d0-a842-5d5982a733dd" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a> anmelden.</p>
