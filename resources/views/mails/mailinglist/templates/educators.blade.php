daeb6de55e035b8a713f5ae26f5244f878bada83
Marcel Stadelmann <m@marceli.to>
2023-06-08 16:16:46 +0200

    wip

--- resources/views/mails/mailinglist/templates/educators.blade.php
+++ resources/views/mails/mailinglist/templates/educators.blade.php
@@ -1,8 +1,8 @@
 @component('mail::message')
 <p>Liebe Kolleginnen und Kollegen</p>
 <p>Gerne machen wir Sie auf freie Plätze im Kurs <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/akute-traumatisierung/c56797ea-d47c-44d0-a842-5d5982a733dd" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">«Akute Traumatisierung» am 21./22. Juli 2023</a> aufmerksam.</p>
-<p>Unser erfahrener Dozent und Leiter des Zentrums für Trauma- und Konfliktmanagement Thomas Weber führt die Teilnehmenden breit in die zentralen Konzepte der Psychotraumatologie ein, so dass sie ein solides Fundament für die Teilnahme an den stärker spezialisierten Kursen des SIPT gewinnen.</p>
-<p>Sie möchten verschiedene Stabilisierungstechniken sowie Massnahmen bei Akutinterventionen kennenlernen?</p>
+<p>Unser erfahrener Dozent und Leiter des Zentrums für Trauma- und Konfliktmanagement Thomas Weber vermittelt und vertieft mit den Teilnehmenden verschiedene Stabilisierungstechniken sowie Massnahmen bei Akutinterventionen. Sie gewinnen ein solides Fundament für viele spezialisierte Kurse des SIPT.</p>
+<p>Sie möchten die zentralen Konzepte der Psychotraumatologie im Kontekt der aktuten Traumatisierung kennenlernen?</p>
 <p>Gerne können Sie sich <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/akute-traumatisierung/c56797ea-d47c-44d0-a842-5d5982a733dd" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a> anmelden.</p>
 <p>Herzliche Grüsse<br>Rosmarie Barwinski</p><br>
 <p style="font-size: 14px; margin-bottom: 0">Sie möchten keine News vom SIPT mehr erhalten? Klicken Sie bitte <span class="break-all"><a href="{{ route('mailinglist_cancel', ['mailinglistSubscriber' => $subscriber->id]) }}" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</span></p>
