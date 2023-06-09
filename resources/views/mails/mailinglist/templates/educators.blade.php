0f60894ac54f9afd57d01162eca97124276c8ec9
Marcel Stadelmann <m@marceli.to>
2023-06-09 16:02:02 +0200

    wip

--- resources/views/mails/mailinglist/templates/educators.blade.php
+++ resources/views/mails/mailinglist/templates/educators.blade.php
@@ -4,7 +4,7 @@
 <p>Unser erfahrener Dozent und Leiter des Zentrums für Trauma- und Konfliktmanagement Thomas Weber vermittelt und vertieft mit den Teilnehmenden verschiedene Stabilisierungstechniken sowie Massnahmen bei Akutinterventionen. Sie gewinnen ein solides Fundament für viele spezialisierte Kurse des SIPT.</p>
 <p>Sie möchten die zentralen Konzepte der Psychotraumatologie im Kontekt der aktuten Traumatisierung kennenlernen?</p>
 <p>Gerne können Sie sich <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/akute-traumatisierung/c56797ea-d47c-44d0-a842-5d5982a733dd" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a> anmelden.</p>
-<p>Das jüngste Interview mit Thomas Weber im Tages-Anzeiger vom 2. Juni 2023 ist zu lesen <a href="https://www.tagesanzeiger.ch/triggerwarnungen-trauma-psychologie-interview-thomas-weber-785758235810" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</p>
+<p>Das jüngste Interview mit Thomas Weber im Tages-Anzeiger vom 2. Juni 2023 ist <a href="https://www.tagesanzeiger.ch/triggerwarnungen-trauma-psychologie-interview-thomas-weber-785758235810" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a> zu lesen.</p>
 <p>Herzliche Grüsse<br>Rosmarie Barwinski</p><br>
 <p style="font-size: 14px; margin-bottom: 0">Sie möchten keine News vom SIPT mehr erhalten? Klicken Sie bitte <span class="break-all"><a href="{{ route('mailinglist_cancel', ['mailinglistSubscriber' => $subscriber->id]) }}" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</span></p>
 @endcomponent
