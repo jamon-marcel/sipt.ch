eea9d6434905f9ac5dbb1a60c53822443ec9c7e2
Marcel Stadelmann <m@marceli.to>
2023-08-16 21:53:54 +0200

    wip

--- resources/views/mails/mailinglist/templates/psychotherapists.blade.php
+++ resources/views/mails/mailinglist/templates/psychotherapists.blade.php
@@ -4,6 +4,6 @@
 <p>SIPT Leiterin Rosmarie Barwinski wird den Teilnehmenden die zentralen Konzepte nach der Mehrdimensionalen Psychodynamischen Traumatherapie (MPTT) anhand von Fallbeispielen praxisnah vermitteln. Den Schwerpunkt legt sie auf die differentielle Indikation und entsprechende Behandlungsplanung.</p>
 <p>Sie möchten Ihre eigene bisherige Praxis zur Behandlung von Traumata besser kennenlernen und reflektiert weiterentwickeln?</p>
 <p>Gerne können Sie sich <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/einfuehrung-in-mptt-mehrdimensionale-psychodynamische-traumatherapie/33056c43-c7b1-4c13-8b13-bb81a97efe6b" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a> anmelden.</p>
-<p>Herzliche Grüsse<br>Rosmarie Barwinski</p><br>
+<p>Beste Grüsse<br>SIPT</p><br>
 <p style="font-size: 14px; margin-bottom: 0">Sie möchten keine News vom SIPT mehr erhalten? Klicken Sie bitte <span class="break-all"><a href="{{ route('mailinglist_cancel', ['mailinglistSubscriber' => $subscriber->id]) }}" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</span></p>
 @endcomponent
