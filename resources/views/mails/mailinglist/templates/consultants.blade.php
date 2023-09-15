d9138d1b602635371b8794311a3f19e0a334b474
Marcel Stadelmann <m@marceli.to>
2023-09-15 14:16:46 +0200

    wip

--- resources/views/mails/mailinglist/templates/consultants.blade.php
+++ resources/views/mails/mailinglist/templates/consultants.blade.php
@@ -8,3 +8,4 @@
 <p>Beste Grüsse<br>SIPT</p><br>
 <p style="font-size: 14px; margin-bottom: 0">Sie möchten keine News vom SIPT mehr erhalten? Klicken Sie bitte <span class="break-all"><a href="{{ route('mailinglist_cancel', ['mailinglistSubscriber' => $subscriber->id]) }}" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</span></p>
 @endcomponent
+
