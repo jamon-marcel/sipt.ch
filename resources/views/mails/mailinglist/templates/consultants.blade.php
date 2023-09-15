c45659c04ce3ed484884b37342d30c642f96451c
Marcel Stadelmann <m@marceli.to>
2023-09-15 14:19:11 +0200

    wip

--- resources/views/mails/mailinglist/templates/consultants.blade.php
+++ resources/views/mails/mailinglist/templates/consultants.blade.php
@@ -4,8 +4,7 @@
 <p>«Trauma und Gegenübertragung: Den Stand der Traumaverarbeitung erkennen und Behandlungsschritte planen» ist ein praxisnaher Leitfaden für alle PsychotherapeutInnen und andere Berufsgruppen, die mit traumatisierten PatientInnen konfrontiert sind.</p>
 <p>Sie lernen Retraumatisierungen wirkungsvoll zu verhindern und zahlreiche auf die Prozessdynamik abgestimmte Interventionen kennen.</p>
 <p>Das Phänomen der Gegenübertragung kann dazu genutzt werden, den Stand im Trauma-Verarbeitungsprozess zu bestimmen und die therapeutische Beziehung entwicklungsfördernd zu gestalten. Wie, das stellt Rosmarie Barwinski konkret in ihrer neuartigen Methode vor.</p>
-<p>Mehr erfahren und <a href="https://bit.ly/3GIttK7" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a> bestellen.</p>
+<p>Mehr erfahren und bestellen: <a href="https://bit.ly/3GIttK7" target="_blank" class="anchor" style="color: #ff7a00; text-decoration: none;">https://bit.ly/3GIttK7 </a></p>
 <p>Beste Grüsse<br>SIPT</p><br>
 <p style="font-size: 14px; margin-bottom: 0">Sie möchten keine News vom SIPT mehr erhalten? Klicken Sie bitte <span class="break-all"><a href="{{ route('mailinglist_cancel', ['mailinglistSubscriber' => $subscriber->id]) }}" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</span></p>
 @endcomponent
-
