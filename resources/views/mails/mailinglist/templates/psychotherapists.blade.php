e338f4db239204d13c934b0071ddf47dd07e21a5
Marcel Stadelmann <m@marceli.to>
2023-10-23 10:54:41 +0200

    wip

--- resources/views/mails/mailinglist/templates/psychotherapists.blade.php
+++ resources/views/mails/mailinglist/templates/psychotherapists.blade.php
@@ -1,4 +1,5 @@
 @component('mail::message')
+<p>Liebe Kolleginnen und Kollegen</p>
 <p>Gerne machen wir Sie auf folgende Tagung aufmerksam, die unsere Dozentin Dr. rer. nat. Birgit Mayer mitorganisiert hat:</p>
 <p><strong>Trauma und Traumafolgestörung bei Menschen mit Beeinträchtigung Diagnostik, Therapie und Begleitung</strong></p>
 <p>Herzliche Grüsse<br>Prof. em. Dr. Rosmarie Barwinski<br>Leitung SIPT</p><br>
