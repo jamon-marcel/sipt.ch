0f9c0875811d7e9ce2a55d00ffa7566ef97f5a9a
Marcel Stadelmann <m@marceli.to>
2023-12-28 10:50:02 +0100

    wip

--- resources/views/mails/mailinglist/templates/psychotherapists.blade.php
+++ resources/views/mails/mailinglist/templates/psychotherapists.blade.php
@@ -3,7 +3,7 @@
 <p>Gerne machen wir Sie auf das Modul <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/stabilisierung-in-der-traumatherapie/0598074b-68e7-4a5c-8eef-88fe012fafc2" class="anchor" style="color: #ff7a00; text-decoration: none;">«Stabilisierung in der Traumatherapie»</a> aufmerksam, welches am 26./27. Januar 2024 stattfindet.</p>
 <p>SIPT-Dozentin und Fachpsychotherapeutin für Psychotraumatologie Martina Frei Liebrich wird Ihnen praxisnah einen Überblick über die verschiedenen Stabilisierungstechniken vermitteln, die während der gesamten Behandlungsdauer von traumatisierten Menschen angewendet werden können. Sie lernen dabei die Funktionen kennen, die die Symptome einer Traumafolgestörung in den verschiedenen Phasen der Verarbeitung von Traumata erfüllen.</p>
 <p>Sie möchten Ihre Qualifikation in der Behandlung von Traumafolgestörungen erhöhen?</p>
-<p>Wir freuen uns über Ihre Anmeldung <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/traum-und-trauma-fakultativ/2058beb2-2ab8-4ce9-b0df-28d257498635" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</p>
+<p>Wir freuen uns über Ihre Anmeldung <a href="https://psychotraumatologie-sipt.ch/bildungsangebot/modul/stabilisierung-in-der-traumatherapie/0598074b-68e7-4a5c-8eef-88fe012fafc2" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</p>
 <p>Beste Grüsse<br>Rosmarie Barwinski</p><br>
 <p style="font-size: 14px; margin-bottom: 0">Sie möchten keine News vom SIPT mehr erhalten? Klicken Sie bitte <span class="break-all"><a href="{{ route('mailinglist_cancel', ['mailinglistSubscriber' => $subscriber->id]) }}" class="anchor" style="color: #ff7a00; text-decoration: none;">hier</a>.</span></p>
 @endcomponent
