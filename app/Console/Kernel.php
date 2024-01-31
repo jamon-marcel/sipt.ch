5e08800ac4c3d6748e1a81df8c37c84e1e19cede
Marcel Stadelmann <m@marceli.to>
2024-01-31 15:18:55 +0100

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends ConsoleKernel
       $schedule->call(new CourseEventInvitations)->everyMinute();
       $schedule->call(new CourseEventReminder)->everyMinute();
       $schedule->call(new Message)->everyMinute();
-      $schedule->call(new Mailing)->everyMinute();
+      // $schedule->call(new Mailing)->everyMinute();
       $schedule->call(new DatabaseBackup)->daily();
     }
   }
