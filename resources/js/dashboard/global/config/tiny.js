9db5ec5b1df849b052070c2f5f6d36022cc7b6b8
Marcel Stadelmann <m@marceli.to>
2024-02-07 14:29:11 +0100

    wip

--- resources/js/dashboard/global/config/tiny.js
+++ resources/js/dashboard/global/config/tiny.js
@@ -7,7 +7,6 @@ export default {
     link: '/assets/dashboard/js/tinymce/plugins/link/plugin.min.js',
   },
   document_base_url : "https://psychotraumatologie-sipt.ch",
-  relative_urls : false,
   convert_urls : false,
   plugins: ["lists"],
   toolbar: 'undo redo | bold | bullist | link | superscript | removeformat | styleselect',
