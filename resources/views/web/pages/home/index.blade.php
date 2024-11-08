fd7fee95410019245ed7669d497c350227dcab7f
Marcel Stadelmann <m@marceli.to>
2024-11-08 08:06:58 +0100

    wip

--- resources/views/web/pages/home/index.blade.php
+++ resources/views/web/pages/home/index.blade.php
@@ -5,11 +5,7 @@
 <section class="theme-light order-1">
   <x-header title="Aktuell" />
 
-  <article>
-    <h2>Veranstaltungshinweise</h2>
-    <p class="word-break">«Vom individuellen und kollektiven Umgang mit traumatischen Erfahrungen» und der Nahostkonflikt ist das Thema des <a href="/storage/downloads/sipt.ch-TNP-Herbstsymposium_2024_Einladung.pdf" target="_blank">TNP-Herbstsymposiums</a> am 7. November 2024 in der Klinik Arlesheim. Es erwarten Sie Referate und Diskussionsrunden mit Prof. Dr. phil. José Brunner und Prof. Dr. phil. Rosmarie Barwinski.</p>
-    <p class="word-break">Der <a href="/storage/downloads/sipt-Poster_International Congress_Switzerland.pdf" target="_blank">International Congress of Psychoanalysis</a> zum Thema «Children, War and Persecutions» findet vom 14.-17. Mai 2025 in Maputo, Mosambik, statt. Unter den Referenten sind Prof. Dr. phil. Rosmarie Barwinski, Dr. phil. Marianne Rauwald, Dr. med. Hubertus Adam und Dr. phil. Boia Efraim. Bei Interesse melden Sie sich gerne unter <a href="mailto:congressmaputo@gmail.com">congressmaputo@gmail.com</a>. Website und detailliertes Programm folgen.</p>
-  </article>
+
 
   <article>
     <h2>Literaturempfehlungen</h2>
