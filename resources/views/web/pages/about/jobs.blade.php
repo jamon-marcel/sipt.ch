9eafc078c78015637c666417c6fa528d32e20497
Marcel Stadelmann <m@marceli.to>
2026-08-14 12:47:30 +0200

    Joblist updates

--- resources/views/web/pages/about/jobs.blade.php
+++ resources/views/web/pages/about/jobs.blade.php
@@ -31,13 +31,6 @@
       </a>
     </p>
     <p>
-      <a href="https://schulemitperspektive.ch/stellen-2/" target="_blank" class="btn-download" title="Psychotherapeut:in mit Schwerpunkt Kinder und Jugendliche, 60%">
-        <strong>
-          Sekundarlehrperson, 80% (SOMOSA-Schule, W/M)
-        </strong>
-      </a> 
-    </p>
-    <p>
       <a href="/storage/downloads/sipt-PsychotherapeutIn-60-80.pdf" target="_blank" class="btn-download" title="Psychotherapeut:in mit Schwerpunkt Kinder und Jugendliche, 60%">
         <strong>
           Psychotherapeut:in 60-80% (Standort Bülach)
@@ -45,46 +38,11 @@
       </a>
     </p>
     <p>
-      <a href="/storage/downloads/sipt-PsychotherapeutIn_Kinder-_und_Jugendbereich_60.pdf" target="_blank" class="btn-download" title="Psychotherapeut:in mit Schwerpunkt Kinder und Jugendliche, 60%">
-        <strong>Psychotherapeut:in mit Schwerpunkt Kinder und Jugendliche, 60%</strong>
-      </a>
-    </p>
-    <p>
-      <a href="/storage/downloads/sipt-PsychotherapeutIn-80.pdf" target="_blank" class="btn-download" title="Psychotherapeut:in 80% (Standort Bülach und Kloten)">
-        <strong>Psychotherapeut:in 80% (Standorte Bülach und Kloten)<br></strong>
-      </a>
-    </p>
-
-    <p>
       <a href="https://jobs.aoz.ch/aoz/job/details/3558601/" target="_blank" class="btn-download" title="Psychotherapeut*in 60-90%">
         <strong>Psychotherapeut*in 60-90%</strong>
       </a>
     </p>
-    <p>
-      <a href="/storage/downloads/sipt-Dipl.-Pflegefachperson-HF_FH-80_2024.pdf" target="_blank" class="btn-download" title="Dipl. Pflegefachperson HF/FH 80%">
-        <strong>Dipl. Pflegefachperson HF/FH 80%</strong>
-      </a>
-    </p>
-    <p>
-      <a href="/storage/downloads/gravita-psychologischen-psychotherapeutin-psychotherapeuten-60-90.pdf" target="_blank" class="btn-download" title="Psychologische/n Psychotherapeutin / Psychotherapeuten 60 – 90 %">
-        <strong>Psychologische/n Psychotherapeutin / Psychotherapeuten 60 – 90 %</strong> [PDF, 119KB]
-      </a>
-    </p>
-    <p>
-      <a href="/storage/downloads/sipt-Inserat_Puent-Sued.pdf" target="_blank" class="btn-download" title="Diplomierte Pflegefachperson">
-        <strong>Diplomierte Pflegefachperson</strong> [PDF, 102KB]
-      </a>
-    </p>
-    <p>
-      <a href="/storage/downloads/sipt-CLL_Dipl_Pflegefachperson_PanC.pdf" target="_blank" class="btn-download" title="Diplomierte Pflegefachperson 80 - 90%">
-        <strong>Diplomierte Pflegefachperson 80 - 90%</strong> [PDF, 87KB]
-      </a>
-    </p>
-    <p>
-      <a href="/storage/downloads/sipt-CLL_Psychologin_PuentSued.pdf" target="_blank" class="btn-download" title="Psychologin / Psychologen 80 - 100%">
-        <strong>Psychologin / Psychologen 80 - 100%</strong> [PDF, 85KB]
-      </a>
-    </p>
+  
   </article>
 </section>
 @endsection
